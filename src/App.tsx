978646e6fb1ebdad1d31a60ae2b5796c4b5ea4fc
Zul Fahri Baihaqi <zurihaqi@gmail.com>
2025-07-02 16:55:26 +0700

    addressing warning

--- src/App.tsx
+++ src/App.tsx
@@ -13,31 +13,33 @@ const App = () => {
       <StatusBar barStyle="dark-content" />
       <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
         <View style={{flex: 1}}>
-          <NativeEngine camera={camera}>
-            <Scene
-              onSceneReady={scene => {
-                scene.createDefaultCameraOrLight(true, undefined, true);
-                const arcCamera = scene.activeCamera as ArcRotateCamera;
+          {camera && (
+            <NativeEngine camera={camera}>
+              <Scene
+                onSceneReady={scene => {
+                  scene.createDefaultCameraOrLight(true, undefined, true);
+                  const arcCamera = scene.activeCamera as ArcRotateCamera;
 
-                arcCamera.panningSensibility = 5000; // Default is 1000, higher = slower
+                  arcCamera.panningSensibility = 5000; // Default is 1000, higher = slower
 
-                arcCamera.pinchPrecision = 3000; // Default is 12, higher = slower
+                  arcCamera.pinchPrecision = 3000; // Default is 12, higher = slower
 
-                arcCamera.wheelPrecision = 100; // Default is 3, higher = slower
+                  arcCamera.wheelPrecision = 100; // Default is 3, higher = slower
 
-                arcCamera.speed = 0.1; // Default is 2, lower = slower
+                  arcCamera.speed = 0.1; // Default is 2, lower = slower
 
-                arcCamera.angularSensibilityX = 4000; // Default is 1000
-                arcCamera.angularSensibilityY = 4000; // Default is 1000
+                  arcCamera.angularSensibilityX = 4000; // Default is 1000
+                  arcCamera.angularSensibilityY = 4000; // Default is 1000
 
-                // Posisi awal camera
-                arcCamera.alpha = Math.PI / 2;
-                arcCamera.beta = Tools.ToRadians(70);
-                setCamera(arcCamera);
-              }}>
-              <Content />
-            </Scene>
-          </NativeEngine>
+                  // Posisi awal camera
+                  arcCamera.alpha = Math.PI / 2;
+                  arcCamera.beta = Tools.ToRadians(70);
+                  setCamera(arcCamera);
+                }}>
+                <Content />
+              </Scene>
+            </NativeEngine>
+          )}
         </View>
       </SafeAreaView>
     </>
